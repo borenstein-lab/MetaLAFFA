80c1c8e90ffe11b5488b06b166a37e5a1b5d255e
engal <engal@optonline.net>
2019-06-24 11:37:38 -0700

    Creating configuration module-based hit filtering step

---
+++ config/steps/hit_filter_combine.py
@@ -0,0 +1,83 @@
+"""
+Hit filter combine step parameters
+---------------------------
+
+This configuration submodule contains parameters related to the hit filter combine pipeline step.
+"""
+
+import config.operation as op
+import config.library_functions as lf
+
+input_dic = {
+    "forward": {"prior_step": 0, "file": "{wildcards.sample}.%s.mapping.txt" % op.fastq_types["forward"]},
+    "reverse": {"prior_step": 0, "file": "{wildcards.sample}.%s.mapping.txt" % op.fastq_types["reverse"]},
+    "singleton": {"prior_step": 0, "file": "{wildcards.sample}.%s.mapping.txt" % op.fastq_types["singleton"]}
+}
+"""
+Dictionary defining the pipeline step's input structure.
+"""
+
+step_prefix = "hit_filter_combine"
+"""
+The prefix to use in output subdirectory naming and provenance naming
+"""
+
+output_list = [
+    "{sample}.mapping.txt"
+]
+"""
+List defining the pipeline step's output structure.
+"""
+
+cluster_params = {}
+"""
+Dictionary defining the pipeline step's cluster parameters
+"""
+
+resource_params = {}
+"""
+Dictionary defining the pipeline step's parameters that control resource usage but do not affect the output
+"""
+
+operating_params = {
+    "type": "default"  # ID for operation to perform
+}
+"""
+Dictionary defining the pipeline step's parameters using when running the associated software
+"""
+
+benchmark_file = "{sample}.log"
+"""
+The benchmark filename pattern
+"""
+
+# Defining options for different operations to run during this step
+
+
+def default(inputs, outputs, wildcards):
+    """
+    Default FASTQ summary operations.
+
+    :param inputs: Object containing the input file names
+    :param outputs: Dictionary containing the output file names
+    :param wildcards: Wildcards determined from input file name patterns
+    :return: None.
+    """
+
+    lf.combine_list_rows(inputs, outputs[0])
+
+
+# Defining the wrapper function that chooses which defined operation to run
+
+def rule_function(inputs, outputs, wildcards):
+    """
+    How to run the software associated with this step
+
+    :param inputs: Object containing the input file names
+    :param outputs: Dictionary containing the output file names
+    :param wildcards: Wildcards determined from input file name patterns
+    :return: None.
+    """
+
+    if operating_params["type"] == "default":
+        default(inputs, outputs, wildcards)
--- src/hit_filter.py
+++ src/hit_filter.py
@@ -1,35 +1,34 @@
-#!/net/borenstein/vol1/PROGRAMS/python2/bin/python
-#
-# Author: Alex Eng
-# Date: 11/27/2017
-
-import argparse,sys
-from file_handling import *
-from future import *
+import argparse
+import sys
+import os
+from file_handling_lib import *
 
 # Parse command line arguments
 parser = argparse.ArgumentParser(description="Filters blast hits based on the selected criterion")
 parser.add_argument("blast_output_file", help="blast output file to filter")
-parser.add_argument("filtering_method", choices=["best_hit", "best_ko", "best_N_hits", "best_N_kos"], help="The filtering criterion to use")
-parser.add_argument("db_version", help="Version (year_month_date) of KEGG to use for filtering hits")
+parser.add_argument("filtering_method", choices=["best_hit", "best_ortholog", "best_N_hits", "best_N_orthologs"], help="The filtering criterion to use")
 parser.add_argument("--number", "-n", type=int, default=20, help="The number of best hits or kos to use")
-parser.add_argument("--path_to_kegg", "-p", default="/net/borenstein/vol1/DATA_REFERENCE/KEGG/", help="Path to KEGG reference data")
+parser.add_argument("--gene_to_ortholog_map", "-g", help="Path to file mapping genes to orthologs")
 parser.add_argument("--output", "-o", help="File to write output to (default: print to standard output)")
-args=parser.parse_args()
+args = parser.parse_args()
 
 # Set output stream
 output = sys.stdout
 if args.output:
 	output = open(args.output, "w")
 
-# If we are using a KO-based criterion, get the set of genes with associated KOs
-ko_genes = set()
-if args.filtering_method in ["best_ko", "best_N_kos"]:
+# If we are using an ortholog-based criterion, get the set of genes with associated orthologs
+ortholog_genes = set()
+if args.filtering_method in ["best_ortholog", "best_N_orthologs"]:
 
-	# Add each gene that appears in the file to the set of genes with ko associations (second column)
-	f = custom_read(args.path_to_kegg + "KEGG_" + args.kegg_version + "/genes/ko/ko_genes.list")
+	# Check if a gene-to-ortholog mapping was provided, exit and report if not
+	if not args.gene_to_ortholog_map:
+		sys.exit("Filter method %s requires a gene-to-ortholog mapping file and none was provided." % args.filtering_method)
+
+	# Add each gene that appears in the file to the set of genes with ortholog associations (second column)
+	f = custom_read(args.args.gene_to_ortholog_map)
 	for line in f:
-		ko_genes.add(line.strip().split()[1])
+		ortholog_genes.add(line.strip().split()[1])
 	f.close()
 
 # Initialize variables used for filtering blast hits
@@ -55,7 +54,7 @@ for line in f:
 
 		# Print the previous read hit information
 		for hit in curr_read_hits:
-			output.write(hit + "\n")
+			output.write(hit + os.linesep)
 
 		# Reset the tracking variables for the new current read
 		curr_read_name = read_name
@@ -63,8 +62,8 @@ for line in f:
 		curr_best_e_val = float("Inf")
 		curr_num_hits = 0
 
-	# If the current e value is less than or equal to the best e value for hits for this read and we're using a best hit method or we're using a best ko method and the gene is associated with a ko then we keep this hit
-	if e_val <= curr_best_e_val and (args.filtering_method in ["best_hit", "best_N_hits"] or (args.filtering_method in ["best_ko", "best_N_kos"] and gene_hit in ko_genes)):
+	# If the current e value is less than or equal to the best e value for hits for this read and we're using a best hit method or we're using a best ortholog method and the gene is associated with a ortholog then we keep this hit
+	if e_val <= curr_best_e_val and (args.filtering_method in ["best_hit", "best_N_hits"] or (args.filtering_method in ["best_ortholog", "best_N_orthologs"] and gene_hit in ortholog_genes)):
 		curr_read_hits.append("\t".join([read_name, gene_hit, str(e_val)]))
 		curr_num_hits += 1
 
@@ -72,14 +71,12 @@ for line in f:
 		if e_val < curr_best_e_val:
 			curr_best_e_val = e_val
 
-	# Otherwise, if the current e value is greater than the best e value, then we only keep it if we are using a best N method, we haven't seen N hits yet, and if we are using the best N ko method then we also check if the gene is associated with a ko
-	elif e_val > curr_best_e_val and (args.filtering_method == "best_N_hits" or (args.filtering_method == "best_N_kos" and gene_hit in ko_genes)) and curr_num_hits < args.number:
+	# Otherwise, if the current e value is greater than the best e value, then we only keep it if we are using a best N method, we haven't seen N hits yet, and if we are using the best N ortholog method then we also check if the gene is associated with aan ortholog
+	elif e_val > curr_best_e_val and (args.filtering_method == "best_N_hits" or (args.filtering_method == "best_N_orthologs" and gene_hit in ortholog_genes)) and curr_num_hits < args.number:
 		curr_read_hits.append("\t".join([read_name, gene_hit, str(e_val)]))
 		curr_num_hits += 1
 
-    # An else is unnecessary here regarding invalid filtering methods because that will be caught by argparse
-
 # Don't forget to print any info for the last read
 for hit in curr_read_hits:
-	output.write(hit + "\n")
+	output.write(hit + os.linesep)
 output.close()
