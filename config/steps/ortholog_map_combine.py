8ce3c7aba9526ed89cdabab1d0ea35ba30ae745a
engal <engal@optonline.net>
2019-06-24 11:39:02 -0700

    Creating configuration module-based gene-to-ortholog mapping step

---
+++ config/steps/ortholog_map_combine.py
@@ -0,0 +1,81 @@
+"""
+Ortholog map combine step parameters
+---------------------------
+
+This configuration submodule contains parameters related to the ortholog map combine pipeline step.
+"""
+
+import config.operation as op
+import subprocess
+
+input_dic = {
+    "input": {"prior_step": 0, "file": "{sample}.orthologs.tab"}
+}
+"""
+Dictionary defining the pipeline step's input structure.
+"""
+
+step_prefix = "ortholog_map_combine"
+"""
+The prefix to use in output subdirectory naming and provenance naming
+"""
+
+output_list = [
+    "orthologs.tab"
+]
+"""
+List defining the pipeline step's output structure.
+"""
+
+cluster_params = {}
+"""
+Dictionary defining the pipeline step's cluster parameters
+"""
+
+resource_params = {}
+"""
+Dictionary defining the pipeline step's parameters that control resource usage but do not affect the output
+"""
+
+operating_params = {
+    "type": "default"  # ID for operation to perform
+}
+"""
+Dictionary defining the pipeline step's parameters using when running the associated software
+"""
+
+benchmark_file = "log"
+"""
+The benchmark filename pattern
+"""
+
+# Defining options for different operations to run during this step
+
+
+def default(inputs, outputs, wildcards):
+    """
+    Default FASTQ summary operations.
+
+    :param inputs: Object containing the input file names
+    :param outputs: Dictionary containing the output file names
+    :param wildcards: Wildcards determined from input file name patterns
+    :return: None.
+    """
+
+    subprocess.run([op.python, "src/summary_combine.py", inputs.input, "--output", outputs[0]])
+
+
+# Defining the wrapper function that chooses which defined operation to run
+
+def rule_function(inputs, outputs, wildcards):
+    """
+    How to run the software associated with this step
+
+    :param inputs: Object containing the input file names
+    :param outputs: Dictionary containing the output file names
+    :param wildcards: Wildcards determined from input file name patterns
+    :return: None.
+    """
+
+    if operating_params["type"] == "default":
+        default(inputs, outputs, wildcards)
