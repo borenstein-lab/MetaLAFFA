435bcaac971d705f17e04a0fdf3c0431a7423c3f
engal <engal@optonline.net>
2019-06-24 11:39:52 -0700

    Creating configuration module-based ortholog aggregation step

---
+++ config/steps/ortholog_aggregation.py
@@ -0,0 +1,94 @@
+"""
+Ortholog aggregation step parameters
+---------------------------
+
+This configuration submodule contains parameters related to the ortholog aggregation pipeline step.
+"""
+
+import config.operation as op
+import config.file_organization as fo
+import config.library_functions as lf
+import subprocess
+
+input_dic = {
+    "input": {"prior_step": 0, "file": "orthologs.tab"}
+}
+"""
+Dictionary defining the pipeline step's input structure.
+"""
+
+step_prefix = "ortholog_aggregation"
+"""
+The prefix to use in output subdirectory naming and provenance naming
+"""
+
+output_list = [
+    "{mapping}.aggregated_orthologs.tab"
+]
+"""
+List defining the pipeline step's output structure.
+"""
+
+cluster_params = {}
+"""
+Dictionary defining the pipeline step's cluster parameters
+"""
+
+resource_params = {}
+"""
+Dictionary defining the pipeline step's parameters that control resource usage but do not affect the output
+"""
+
+operating_params = {
+    "type": "default",  # ID for operation to perform
+    "method": "",  # Method to use for ortholog aggregation, options include: standard, empanada
+    "standard_method": "fractional"  # Standard aggregation method to use, options include: fractional, whole
+}
+"""
+Dictionary defining the pipeline step's parameters using when running the associated software
+"""
+
+benchmark_file = "{mapping}.log"
+"""
+The benchmark filename pattern
+"""
+
+# Defining options for different operations to run during this step
+
+
+def default(inputs, outputs, wildcards):
+    """
+    Default gene map operations.
+
+    :param inputs: Object containing the input file names
+    :param outputs: Dictionary containing the output file names
+    :param wildcards: Wildcards determined from input file name patterns
+    :return: None.
+    """
+
+    # If the input file is non-empty, map the reads
+    if not lf.is_empty(inputs.input):
+
+        mapping = fo.ortholog_to_grouping_directory + wildcards.mapping
+        if operating_params["method"] == "standard":
+            subprocess.run([op.python, "src/ortholog_aggregation.py", inputs.input, operating_params["standard_method"], mapping, "--output", outputs[0]])
+
+    # Otherwise, if the input file is a dummy file, create dummy outputs
+    else:
+        subprocess.run(["touch", outputs[0]])
+
+
+# Defining the wrapper function that chooses which defined operation to run
+
+def rule_function(inputs, outputs, wildcards):
+    """
+    How to run the software associated with this step
+
+    :param inputs: Object containing the input file names
+    :param outputs: Dictionary containing the output file names
+    :param wildcards: Wildcards determined from input file name patterns
+    :return: None.
+    """
+
+    if operating_params["type"] == "default":
+        default(inputs, outputs, wildcards)
---
+++ src/ortholog_aggregation.py
@@ -0,0 +1,40 @@
+import pandas
+import argparse
+
+parser = argparse.ArgumentParser(description="Aggregates ortholog profiles to a higher functional grouping")
+parser.add_argument("ortholog_profiles", help="The table of ortholog profiles to aggregate")
+parser.add_argument("summary_method", choices=["fractional", "whole"], help="The method to use to map orthologs to the higher functional grouping")
+parser.add_argument("ortholog_to_grouping", help="The file mapping orthologs to groups defined at a higher functional level")
+parser.add_argument("--grouping_name", "-n", default="group", help="The name of the grouping that orthologs are being aggregated into (e.g. module, pathway, etc.), will be used as the column header for grouping IDs (default: %(default)s)")
+parser.add_argument("--output", "-o", default=None, help="File to write output to (default: print to standard output)")
+args = parser.parse_args()
+
+# Read the ortholog profiles and mapping matrix
+ortholog_profiles = pandas.read_table(args.ortholog_profiles, sep="\t", header=0, index_col=0)
+mapping_matrix = pandas.read_csv(args.ortholog_to_grouping, sep="\t", header=0, index_col=0)
+
+# Filter and sort the mapping matrix
+mapping_matrix = mapping_matrix.reindex(list(ortholog_profiles.index))
+
+# If using the fractional summary method, we divide ortholog contributions to higher functional groups evenly among the higher groups each ortholog belongs to
+if args.summary_method == "fractional":
+
+    # Divide each row by the sum of the row
+    mapping_matrix = mapping_matrix.div(mapping_matrix.sum(axis=1), axis=0)
+    mapping_matrix = mapping_matrix.fillna(0)
+
+# Transpose the mapping matrix
+mapping_matrix = mapping_matrix.transpose()
+
+# Multiply the mapping matrix and ortholog profiles
+output_table = mapping_matrix.dot(ortholog_profiles)
+
+# Remove any row that is all zeros
+output_table = output_table[(output_table.T != 0).any()]
+
+# Print the output
+output_string = output_table.to_csv(args.output, sep="\t", header=True, index=True, index_label=args.grouping_name)
+
+# If no output file was specified, then we print the table to standard output
+if not args.output:
+    print(output_string)
