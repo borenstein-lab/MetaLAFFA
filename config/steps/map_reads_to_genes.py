0a365b52b1390c2dd9265288e49323599ee1e7f2
engal <engal@optonline.net>
2019-06-24 11:37:02 -0700

    Creating configuration module-based read mapping step

---
+++ config/steps/map_reads_to_genes.py
@@ -0,0 +1,124 @@
+"""
+Map reads to genes step parameters
+---------------------------
+
+This configuration submodule contains parameters related to the map reads to genes pipeline step.
+"""
+
+import config.operation as op
+import config.file_organization as fo
+import config.library_functions as lf
+import subprocess
+
+input_dic = {
+    "input": {"prior_step": 0, "file": "{wildcards.sample}.{wildcards.type}.fastq"}
+}
+"""
+Dictionary defining the pipeline step's input structure.
+"""
+
+step_prefix = "map_reads_to_genes"
+"""
+The prefix to use in output subdirectory naming and provenance naming
+"""
+
+output_list = [
+    "{sample}.{type}.mapping.txt"
+]
+"""
+List defining the pipeline step's output structure.
+"""
+alignment_memory = 220
+"""
+Amount of RAM (in GB) required for each individual alignment cluster job. 
+"""
+
+alignment_cpus = 22
+"""
+Number of CPUs required for each individual alignment cluster job. 
+"""
+
+alignment_merging_disk_space = 250
+"""
+Amount of local disk space (in GB) required in the temporary file directory for merging alignment outputs. 
+"""
+
+alignment_filtering_disk_space = 200
+"""
+Amount of local disk space (in GB) required in the temporary file directory for filtering alignment output.
+"""
+
+cluster_params = {
+    "memory": "220G",
+    "cpus": 22,
+    "options": "-R y"
+}
+"""
+Dictionary defining the pipeline step's cluster parameters
+"""
+
+resource_params = {
+    "diamond": "src/diamond-0.9.24/bin/diamond",  # Location of the DIAMOND program
+    "block_size": 36,  # Block size to use in DIAMOND
+    "index_chunks": 1  # Index chunks for DIAMOND
+}
+"""
+Dictionary defining the pipeline step's parameters that control resource usage but do not affect the output
+"""
+
+operating_params = {
+    "type": "default",  # ID for operation to perform
+    "method": "blastx",  # Alignment method
+    "top_percentage": 1,  # Keep hits within X percent of the best score
+    "evalue_cutoff": 0.001,  # Keep hits with e-value X or better
+    "sensitivity": ""  # Sensitivity of DIAMOND mapping
+}
+"""
+Dictionary defining the pipeline step's parameters using when running the associated software
+"""
+
+benchmark_file = "{sample}.{type}.log"
+"""
+The benchmark filename pattern
+"""
+
+# Defining options for different operations to run during this step
+
+
+def default(inputs, outputs, wildcards):
+    """
+    Default FASTQ summary operations.
+
+    :param inputs: Object containing the input file names
+    :param outputs: Dictionary containing the output file names
+    :param wildcards: Wildcards determined from input file name patterns
+    :return: None.
+    """
+
+    # Set locations of reference files
+    target_database = fo.database_directory + op.target_database + ".dmnd"
+
+    # If the input file is non-empty, map the reads
+    if not lf.is_empty(inputs.input):
+
+        subprocess.run([resource_params["diamond"], operating_params["method"], "--block-size", str(resource_params["block_size"]), "--index-chunks", str(resource_params["index_chunks"]), "--threads", str(cluster_params["cpus"] * 2), "--target_database", target_database, "--query", inputs.input, "--out", outputs[0], "--top", str(operating_params["top_percentage"]), "--evalue", str(operating_params["evalue_cutoff"]), operating_params["sensitivity"]])
+
+    # Otherwise, if the input file is a dummy file, create a dummy output
+    else:
+        subprocess.run(["touch", outputs[0]])
+
+
+# Defining the wrapper function that chooses which defined operation to run
+
+def rule_function(inputs, outputs, wildcards):
+    """
+    How to run the software associated with this step
+
+    :param inputs: Object containing the input file names
+    :param outputs: Dictionary containing the output file names
+    :param wildcards: Wildcards determined from input file name patterns
+    :return: None.
+    """
+
+    if operating_params["type"] == "default":
+        default(inputs, outputs, wildcards)
