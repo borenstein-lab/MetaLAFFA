ad8ee988ca968fdac78344487231b2ff2d76e21d
engal <engal@optonline.net>
2019-06-25 13:12:48 -0700

    Fixed typo

--- src/create_uniref_gene_to_ortholog.py
+++ src/create_uniref_gene_to_ortholog.py
@@ -41,7 +41,7 @@ with custom_read(args.uniref_id_mapping) as full_mapping:
 # For each ortholog, write a line for each UniRef version ID that maps to it
 for ortholog in ortholog_to_id.keys():
     for uniref_id in ortholog_to_id[ortholog]:
-        if uniref_version_id in id_to_uniref_version:
+        if uniref_id in id_to_uniref_version:
             for uniref_version_id in id_to_uniref_version[uniref_id]:
                 output.write("\t".join([ortholog, uniref_version_id]) + os.linesep)
 
