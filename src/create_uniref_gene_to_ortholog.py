8d0a18dca0d9ff5294e05a2ed0e4f8e8b25e9cde
engal <engal@optonline.net>
2019-08-05 22:19:37 +0300

    Now standardizes tag checking to format to lowercase before the check

--- src/create_uniref_gene_to_ortholog.py
+++ src/create_uniref_gene_to_ortholog.py
@@ -6,10 +6,14 @@ from file_handling_lib import *
 # Parse command line arguments
 parser = argparse.ArgumentParser(description="Using a UniRef ID mapping file, creates a table mapping UniRef gene IDs to ortholog groups.")
 parser.add_argument("uniref_id_mapping", help="File from UniRef mapping gene IDs to various annotations")
-parser.add_argument("uniref_version", help="The version of the UniRef database to match orthologs to (e.g. UniRef90)")
-parser.add_argument("ortholog_type", help="The identifier for the type of ortholog to map genes to (e.g. KO, eggNOG)")
+parser.add_argument("uniref_version", help="The version of the UniRef database to match orthologs to (e.g. uniref90)")
+parser.add_argument("ortholog_type", help="The identifier for the type of ortholog to map genes to (e.g. ko, eggnog)")
 parser.add_argument("--output", "-o", help="File to write output to (default: print to standard output)")
 args = parser.parse_args()
+
+# Standardize strings to be lower-case
+args.uniref_version = args.uniref_version.lower()
+args.ortholog_type = args.ortholog_type.lower()
 
 # Open the output stream
 # Set output stream
@@ -27,13 +31,13 @@ with custom_read(args.uniref_id_mapping) as full_mapping:
         mapping_info = line.strip().split()
 
         # If the mapping type matches the requested UniRef version, add this mapping to the id-to-uniref version mapping
-        if mapping_info[1] == args.uniref_version:
+        if mapping_info[1].lower() == args.uniref_version:
             if mapping_info[0] not in id_to_uniref_version:
                 id_to_uniref_version[mapping_info[0]] = []
             id_to_uniref_version[mapping_info[0]].append(mapping_info[2])
 
         # If the mapping type matches the requested ortholog type, add this mapping to the ortholog-to-id mapping
-        if mapping_info[1] == args.ortholog_type:
+        if mapping_info[1].lower() == args.ortholog_type:
             if mapping_info[2] not in ortholog_to_id:
                 ortholog_to_id[mapping_info[2]] = []
             ortholog_to_id[mapping_info[2]].append(mapping_info[0])
