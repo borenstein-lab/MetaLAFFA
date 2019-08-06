8bfd588668f29a2f825efb47e8e8bfae40a0a073
engal <engal@optonline.net>
2019-08-06 17:20:54 +0300

    Moving import of steps (which depend on config) to avoid circular dependencies

--- config/__init__.py
+++ config/__init__.py
@@ -4,7 +4,6 @@ import os
 from .cluster import *
 from .file_organization import *
 from .operation import *
-from .steps import *
 
 # Create a configured environment
 env = os.environ.copy()
@@ -21,3 +20,5 @@ if "PYTHONPATH" not in env:
 # Otherwise, prepend this custom python path to the list of python paths
 else:
     env["PYTHONPATH"] = fixed_python_path + ":" + env["PYTHONPATH"]
+
+from .steps import *
